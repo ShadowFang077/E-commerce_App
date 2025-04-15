73d9748a6e790774a990b827bff3e3dcad099fff
ShadowFang077 <msivashankar1607@gmail.com>
2025-04-15 11:53:36 +0530

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:e_commerce_application/firebase_options.dart';
 import 'package:e_commerce_application/app/view/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+ // hi
 Future<void> main() async {
    WidgetsFlutterBinding.ensureInitialized(); // this is used to wait for the flutter to initialzed the flutter engine
    await Firebase.initializeApp(options: 
